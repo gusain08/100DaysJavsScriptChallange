b40ef4ae2f6305767841c0d3aa646470fe9e8384
Surendra Singh <surrigusain8@gmail.com>
2023-06-30 00:41:17 +0530

    Second Commit

--- CopyText/script.js
+++ CopyText/script.js
@@ -5,15 +5,9 @@ btn.addEventListener('click',Copytext);
 
 function Copytext(e){
     e.preventDefault();
-    
-    
     word.select();  
     word.setSelectionRange(0, 9999);
     document.execCommand('copy');
-   
-
-
-
     setTimeout(()=>{
         btn.textContent =  'Copy';
 
---
+++ Input Character Counter/script.js
@@ -0,0 +1,8 @@
+let count  = document.querySelector('.count ');
+let input =  document.querySelector('.form-control');
+
+input.addEventListener("keyup",()=>{
+  count.innerHTML  = input.value.length;
+  
+
+});
